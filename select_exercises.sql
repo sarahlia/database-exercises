274d5d2f44fa378b40aeee15de957782ef301180
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-26 16:10:31 -0500

    edit select_exercises after walkthrough

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,14 +1,16 @@
 USE codeup_test_db;
 
+SELECT 'Name of all albums by Pink Floyd:' AS 'Problem 1';
 SELECT name FROM albums where artist = 'Pink Floyd';
 
+SELECT 'Release date of the album Sgt. Pepper''s Lonely Hearts Club Band:' AS 'Problem 2';
 SELECT release_date FROM albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT genre FROM albums where name = 'Nevermind';
 
-SELECT name FROM albums where release_date >= 1990 and release_date < 2000;
+SELECT name FROM albums where release_date BETWEEN 1990 and 1999;
 
-SELECT name FROM albums where sales <= 20;
+SELECT name FROM albums where sales < 20;
 
 SELECT name FROM albums where genre = 'Rock';
 
