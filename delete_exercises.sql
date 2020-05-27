8d4326f87b5c569f193dd6b157c56af0785bdf11
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-27 09:48:19 -0500

    finish delete_exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 
 SELECT 'All albums released after 1991:' AS '';
-SELECT name FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'All disco albums:' AS '';
-SELECT name FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 
 SELECT 'All albums by Celine Dion:' AS '';
-SELECT name FROM albums WHERE artist = 'Celine Dion';
+DELETE FROM albums WHERE artist = 'Celine Dion';
