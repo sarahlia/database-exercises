12aa05a71b064868ec52a76269b759f75d4b91d7
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-27 15:54:57 -0500

    minor edit to exercise after walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -15,9 +15,14 @@ SELECT *
     FROM employees
     WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
+# or the above can be written like this:
+# SELECT *
+# FROM employees
+# WHERE hire_date LIKE '199%';
+
 SELECT *
     FROM employees
-    WHERE birth_date LIKE '%-12-25';
+    WHERE birth_date LIKE '%12-25';
 
 SELECT *
     FROM employees
@@ -36,8 +41,8 @@ WHERE (last_name LIKE 'E%'
 
 SELECT *
     FROM employees
-    WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-    AND birth_date LIKE '%-12-25';
+    WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%12-25';
 
 SELECT *
     FROM employees
