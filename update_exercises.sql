36035fcf80d4b366d9e1f59bfc93e60f5836fbcf
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-27 10:18:25 -0500

    edit update_exercises after walkthrough

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,20 +1,23 @@
 USE codeup_test_db;
 
-# SELECT 'All albums with a lot more sales' AS '';
+SELECT 'All albums with a lot more sales' AS '';
 # SELECT * FROM albums;
-# UPDATE albums
-# SET sales = (sales * 10);
+UPDATE albums
+SET sales = (sales * 10);
 # SELECT * FROM albums;
 
-# SELECT 'All albums with a release date in the 1800s:' AS '';
-# SELECT name FROM albums where release_date < 1980;
-# UPDATE albums
-# SET release_date = (release_date - 100);
+SELECT 'All albums with a release date in the 1800s:' AS '';
+# SELECT * FROM albums where release_date < 1980;
+UPDATE albums
+SET release_date = (release_date - 100)
+WHERE release_date < 1980;
 # SELECT * FROM albums;
 
 SELECT 'All albums by Peter Jackson:' AS '';
-SELECT name FROM albums where artist = 'Michael Jackson';
+# SELECT * FROM albums where artist = 'Michael Jackson';
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
-SELECT name FROM albums where artist = 'Peter Jackson';
+# SELECT name FROM albums where artist = 'Peter Jackson';
+
+SELECT * FROM albums;
