b408af6ffc8e484d889c65966169bb9a91990780
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-29 14:15:25 -0500

    finish all exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -23,3 +23,12 @@ JOIN dept_emp AS de ON de.emp_no = t.emp_no
 JOIN departments AS d ON d.dept_no = de.dept_no
 WHERE (d.dept_name = 'Customer Service' AND t.to_date = '9999-01-01' AND de.to_date = '9999-01-01')
 GROUP BY Title;
+
+# Exercise 5
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Name', s.salary
+FROM employees AS e
+JOIN dept_manager as dm ON dm.emp_no = e.emp_no
+JOIN departments AS d ON d.dept_no = dm.dept_no
+JOIN salaries AS s ON s.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+ORDER BY `Department Name`;
