d8320037975ce849c58d1efd1465cd02f1dff1b7
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-30 22:49:42 -0500

    add an alternative method to solve Exercise 2

--- home-practice/additional_exercises.sql
+++ home-practice/additional_exercises.sql
@@ -8,11 +8,16 @@ ORDER BY Results DESC
 LIMIT 3;
 
 #Exercise 2: lowest employee number for the first senior engineers in the company
-SELECT emp_no, title
+SELECT emp_no
 FROM titles
 WHERE title = 'Senior Engineer'
 ORDER BY emp_no
 LIMIT 1;
+
+#alternatively, we can use MIN for Exercise 2:
+SELECT MIN(emp_no) AS 'Lowest employee number for a Senior Engineer'
+from titles
+WHERE title = 'Senior Engineer';
 
 #Exercise 3: the most common 'from date' for job titles?
 SELECT from_date, COUNT(from_date) AS 'Results'
