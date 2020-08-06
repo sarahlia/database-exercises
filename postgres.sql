2a842d1b0585186c09b417d219c9d5dd07b8bc8e
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-08-06 13:10:38 -0500

    finish PostgresSQL exercise part 1.

---
+++ postgres.sql
@@ -0,0 +1,47 @@
+SELECT occurred_at, account_id, channel
+FROM web_events
+
+# return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at
+LIMIT 10;
+
+# return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 5;
+
+# return the lowest 20 orders in terms of smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 20;
+
+# Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order)
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id, total_amt_usd DESC;
+
+# Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC, account_id;
+
+# Pull the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000
+SELECT * FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
+
+# Pull the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500.
+SELECT * FROM orders
+WHERE gloss_amt_usd < 500
+LIMIT 10;
+
+# Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for the Exxon Mobil company in the accounts table
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
+
