9f68c80ee20359cfafd31cb20a8f46a0ec67f87e
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-08-06 15:02:57 -0500

    finish PostgresSQL exercise part 2.

--- postgres.sql
+++ postgres.sql
@@ -44,4 +44,34 @@ SELECT name, website, primary_poc
 FROM accounts
 WHERE name = 'Exxon Mobil';
 
+# Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the results to the first 10 orders, and include the id and account_id fields.
+SELECT id, account_id, (standard_amt_usd/standard_qty) AS unit_price
+FROM orders
+LIMIT 10;
+
+# Write a query that finds the percentage of revenue that comes from poster paper for each order. You will need to use only the columns that end with _usd. (Try to do this without using the total column.) Display the id and account_id fields also. NOTE - you will receive an error with the correct solution to this question. This occurs because at least one of the values in the data creates a division by zero in your formula.
+SELECT id, account_id, (poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd))*100 AS pct_of_revenue
+FROM orders
+LIMIT 10;
+
+# Find all the companies whose names start with 'C' from the accounts table
+SELECT name FROM accounts
+WHERE name LIKE 'C%';
+
+# all companies whose names contain the string 'one' somewhere in the name
+SELECT name FROM accounts
+WHERE name LIKE '%one%';
+
+# all companies whose names end with 's'
+SELECT name FROM accounts
+WHERE name LIKE '%s';
+
+# use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom
+SELECT name, primary_poc, sales_rep_id FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+# use the web_events table to find all information regarding individuals who were contacted via the channel of organic or adwords
+SELECT * FROM web_events
+WHERE channel IN ('organic', 'adwords');
+
 
